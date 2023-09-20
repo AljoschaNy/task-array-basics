e2170a591b783e3b8d5ffd8c7bcdd1f6595c5128
Aljoscha Nyang <a.nyang@gmx.de>
2023-09-20 11:36:52 +0200

    Task 3: Find biggest number

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         // Schritt 1: Erstelle ein Array mit den Zahlen von 1 bis 10 und gib es auf der Konsole aus.
-        int[] numbersArray1 = {1,2,3,4,5,6,7,8,9,10};
+        int[] numbersArray1 = {1,2,3,4,99,6,7,8,9,10};
 
         for(int number: numbersArray1) {
             System.out.println(number);
@@ -17,6 +17,16 @@ public class Main {
         System.out.println("Summe: " + sum);
 
         // Schritt 3: Finde die größte Zahl im Array und gib sie auf der Konsole aus.
+        int biggestNumber = numbersArray1[0];
+
+        for(int number: numbersArray1) {
+            if(number > biggestNumber) {
+                biggestNumber = number;
+            }
+        }
+
+        System.out.println("The biggest number is: " + biggestNumber);
+
         // Schritt 4: Erstelle ein zweites Array mit den Zahlen von 11 bis 20.
         // Schritt 5: Führe eine elementweise Addition beider Arrays durch und speichere das Ergebnis in einem dritten Array. Gib das Ergebnis-Array auf der Konsole aus.
 
