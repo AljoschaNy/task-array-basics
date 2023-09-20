a86469430971f58a448b4414a138811cb372cb40
Aljoscha Nyang <a.nyang@gmx.de>
2023-09-20 11:21:57 +0200

    Create first numbers array and print to console

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,18 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Schritt 1: Erstelle ein Array mit den Zahlen von 1 bis 10 und gib es auf der Konsole aus.
+        int[] numbersArray1 = {1,2,3,4,5,6,7,8,9,10};
+
+        for(int number: numbersArray1) {
+            System.out.println(number);
+        }
+
+        // Schritt 2: Berechne die Summe aller Zahlen im Array und gib das Ergebnis auf der Konsole aus.
+        // Schritt 3: Finde die größte Zahl im Array und gib sie auf der Konsole aus.
+        // Schritt 4: Erstelle ein zweites Array mit den Zahlen von 11 bis 20.
+        // Schritt 5: Führe eine elementweise Addition beider Arrays durch und speichere das Ergebnis in einem dritten Array. Gib das Ergebnis-Array auf der Konsole aus.
+
+
     }
 }
