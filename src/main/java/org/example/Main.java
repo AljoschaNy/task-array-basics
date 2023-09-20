b5d90d4d48cac0b5b5352094359ca8b006bf9fb5
FabianMariusLink <fabian.marius.link@gmail.com>
2023-09-20 11:28:32 +0200

    Task 2: add numbers

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,6 +10,12 @@ public class Main {
         }
 
         // Schritt 2: Berechne die Summe aller Zahlen im Array und gib das Ergebnis auf der Konsole aus.
+        int sum = 0;
+        for(int number: numbersArray1) {
+            sum += number;
+        }
+        System.out.println("Summe: " + sum);
+
         // Schritt 3: Finde die größte Zahl im Array und gib sie auf der Konsole aus.
         // Schritt 4: Erstelle ein zweites Array mit den Zahlen von 11 bis 20.
         // Schritt 5: Führe eine elementweise Addition beider Arrays durch und speichere das Ergebnis in einem dritten Array. Gib das Ergebnis-Array auf der Konsole aus.
